8b17a5144f3558f70a60e008306196bee4b617ff
Adrian Medina <adrian@wikipunk.net>
2023-01-19 13:49:23 -0500

    add temple component with :init-ns option to resolve init fns

--- src/net/wikipunk/temple.clj
+++ src/net/wikipunk/temple.clj
@@ -6,7 +6,8 @@
    [clojure.set :as set]
    [com.stuartsierra.component :as com]
    [net.wikipunk.boot]
-   [net.wikipunk.ext]))
+   [net.wikipunk.ext]
+   [net.wikipunk.rdf :as rdf]))
 
 (def ^:dynamic *tree-of-life*
   "A Clojure multimethod hierarchy combining rdf/*classes*,
@@ -16,3 +17,20 @@
 (def ^:dynamic *indexes*
   "A map of useful indexes maintained by agents called priests."
   {})
+
+(defrecord Temple [vocab rdf init-ns]
+  com/Lifecycle
+  (start [this]
+    (require init-ns :reload)
+    (let [metaobjects ((ns-resolve init-ns 'setup-canvas))
+          indexes     ((ns-resolve init-ns 'setup-indexes))]
+      (alter-var-root #'*tree-of-life* (constantly metaobjects))
+      (alter-var-root #'*indexes* (constantly indexes))
+      ((ns-resolve init-ns 'finalize))
+      (when-some [conn (:conn rdf)]
+        (rdf/bootstrap conn))
+      (assoc this :metaobjects metaobjects :indexes indexes)))
+  (stop [this]
+    (alter-var-root #'*tree-of-life* (constantly (make-hierarchy)))
+    (alter-var-root #'*indexes* (constantly {}))
+    (assoc this :metaobjects nil :indexes nil)))
